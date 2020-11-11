5f3b73e27edc4ddb03362a5555cd152cac72a48a
강윤호 <rkddbswkd@game.hoseo.edu>
2020-11-11 21:56:18 +0900

    마우스 클릭 & 카메라 이동

---
+++ Source/YutnoriDefense/MouseGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MouseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YUTNORIDEFENSE_API AMouseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
